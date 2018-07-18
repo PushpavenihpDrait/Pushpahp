0bb0600df92ee439cc74e5f33d76767e16029492
Pushpaveni H p <pushpaveni42@yahoo.com>
2018-07-18 11:37:08 +0530

    hello

---
+++ hello.py
@@ -0,0 +1,13 @@
+print "hello world"
+D -> demo-repository ->hello.py
+git status
+git add<filename>
+git add hello.py
+
+git comit -m <comming message here>
+git commit -m "my first file comited"
+
+git config --global user.name "Pushpaveni H p"
+git config --golbal user.email "pushapveni42@yahoo.com"
+
+git push origin master
